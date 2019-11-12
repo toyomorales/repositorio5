4b5a86945dbf2605e5e5a464f2252a240dc4fb1d
toyomorales <54047318+toyomorales@users.noreply.github.com>
2019-11-11 20:05:04 -0500

    se sumaron tres números

--- src/repositorio/Principal.java
+++ src/repositorio/Principal.java
@@ -5,11 +5,12 @@ package repositorio;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        int valor1=0,valor2=0,resultado;
+        int valor1=0,valor2=0,valor3=0,resultado;
         valor1=2;
         valor2=4;
-        
-        resultado=valor1+valor2;
+        valor3=3;
+        resultado=valor1+valor2+valor3;
         System.out.println("LA sumae es: "+resultado);
+        
     }
 }
