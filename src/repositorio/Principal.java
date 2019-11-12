a8c882453c74916b5ab7939489ee6951b951152d
Usuario <Usuario@DESKTOP-8FRKNHM>
2019-11-11 19:58:57 -0500

    Realice una suma

--- src/repositorio/Principal.java
+++ src/repositorio/Principal.java
@@ -5,5 +5,11 @@ package repositorio;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        int valor1=0,valor2=0,resultado;
+        valor1=2;
+        valor2=4;
+        
+        resultado=valor1+valor2;
+        System.out.println("LA sumae es: "+resultado);
     }
 }
